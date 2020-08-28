24328b3b1c886151ade0fb83e7be0805d6d1fd2b
Shravanthi <57304916+shravanthi94@users.noreply.github.com>
2020-08-28 09:23:56 -0700

    Added routers with API GET requests

--- server.js
+++ server.js
@@ -2,12 +2,19 @@ const express = require("express");
 const connectDB = require("./config/db");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
+//connect to the database
 connectDB();
 
 app.get("/", (req, res) => {
   res.send("app running successfully");
 });
 
+//API routes for GET
+app.use("/api/users", require("./routers/api/users"));
+app.use("/api/auth", require("./routers/api/auth"));
+app.use("/api/profile", require("./routers/api/profile"));
+app.use("/api/posts", require("./routers/api/posts"));
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`App started running on ${PORT}`));
