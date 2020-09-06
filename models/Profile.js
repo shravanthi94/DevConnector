289d3bb2ca8a48e587890424b579deae456e68fa
Shravanthi <57304916+shravanthi94@users.noreply.github.com>
2020-09-05 20:59:36 -0700

    Added current user profile route

--- models/Profile.js
+++ models/Profile.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const ProfileSchema = new mongoose.model({
+const ProfileSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "user"
