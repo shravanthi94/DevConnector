dc662019dd206194348c0548c8c771a05773ff4b
Shravanthi <57304916+shravanthi94@users.noreply.github.com>
2020-09-20 14:44:35 -0700

    Added Posts model

---
+++ models/Post.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const User = require('./User');
+
+const PostSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users',
+      },
+    },
+  ],
+  comment: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users',
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Posts = mongoose.model('post', PostSchema);
